1612c47250763aaed83e763517d329437aa66483
Yash-Raj-5424 <myash3499@gmail.com>
2026-01-04 11:37:51 +0530

    enabled transaction management

--- journalApp/src/main/java/com/company/journalApp/services/JournalEntryService.java
+++ journalApp/src/main/java/com/company/journalApp/services/JournalEntryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,14 +23,19 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-
-            User user = userService.findByUserName(userName);
-            journalEntry.setDate(LocalDateTime.now());
-            JournalEntry saved = journalEntryRepository.save(journalEntry);
-            user.getJournalEntries().add(saved);    // save the journal entries into the list of journal entries in user entity
-            userService.saveEntry(user);
-
+            try {
+                User user = userService.findByUserName(userName);
+                journalEntry.setDate(LocalDateTime.now());
+                JournalEntry saved = journalEntryRepository.save(journalEntry);
+                user.getJournalEntries().add(saved);    // save the journal entries into the list of journal entries in user entity
+                userService.saveEntry(user);
+            }
+            catch (Exception e){
+                System.out.println(e);
+                throw new RuntimeException("Some error occured while saving the entry: ", e);
+            }
     }
 
     public void saveEntry(JournalEntry journalEntry){
